fe683c97b94acf118f98eea5e36dfb50be63bc7d
ryutaro.higashi <xxx@gmail.com>
2020-08-20 07:36:45 +0000

    set rsepc

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,13 @@ module HowToGo
 
     config.generators do |g|
       g.assets false
-      g.test_framework false
+      
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      
     end
     
   end
